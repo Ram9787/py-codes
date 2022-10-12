8131a6f58a2b5eb553f71e1aaee9e2439d0cb8f0
Ram9787 <87652671+Ram9787@users.noreply.github.com>
2022-10-12 10:14:44 +0530

    Update 01).Bubble_sort.py

--- 01).Bubble_sort.py
+++ 01).Bubble_sort.py
@@ -6,3 +6,21 @@ for i in range(len(l)):
     
     
 print(l)
+
+#------------------------------------
+#bubble sort when list is in ascending order
+l=[1,2,3,4,5]
+n=len(l)
+f=0
+c=0
+for i in range(n):
+    for j in range(n-i-1):
+        c+=1
+        if(l[j]>l[j+1]):
+            l[j+1],l[j]=l[j],l[j+1]
+            f=1
+            
+    if(f==0):
+        break
+    
+print(l,c)
